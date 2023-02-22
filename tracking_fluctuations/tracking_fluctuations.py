b7695dcc014c7a654ad2344b10c1a41bce672eb4
klimrus61 <klimrus61@mail.ru>
2023-02-22 15:04:49 +0300

    refractor: метод не приватен, подсчет среднего значения с помощью count

--- tracking_fluctuations/tracking_fluctuations.py
+++ tracking_fluctuations/tracking_fluctuations.py
@@ -17,20 +17,23 @@ class TrackingFluctuations():
         Получаем среднюю сумму за предыдущие 7 дней. 
         '''
         average_previous_seven_days_sum = 0
-        last_seven_days_values = []
+        last_seven_days_sum = 0
+        count_days_of_week_with_values = 0
+        count_days = 0
         row_values = self.actual_worksheet.row_values(tracked_cell.row)
         yesterday = tracked_cell.col - 2 - 1
         for col_index in range(yesterday, 1, -2):
             if not row_values[col_index]:
-                last_seven_days_values.append(0)
+                count_days += 1
             else:
-                last_seven_days_values.append(
-                    float(row_values[col_index].replace(',', '.')))
+                last_seven_days_sum += float(row_values[col_index].replace(',', '.'))
+                count_days_of_week_with_values += 1
+                count_days += 1
 
         previous_worksheet_index = self.actual_worksheet.index - 1
         actual_param_name = row_values[1]
 
-        while previous_worksheet_index >= 0 and len(last_seven_days_values) < 7:
+        while previous_worksheet_index >= 0 and count_days < 7:
             worksheet = self.sh.get_worksheet(previous_worksheet_index)
             user_and_app_names = worksheet.col_values(1)
             if user_name not in user_and_app_names or app_name not in user_and_app_names:
@@ -40,25 +43,26 @@ class TrackingFluctuations():
             actual_row_index = None
             for row_index in range(app_name_cell.row-1, app_name_cell.row + 5):
                 if param_names[row_index] == actual_param_name:
-                    actual_row_index = row_index
+                    actual_row_index = row_index + 1
                     break
             worksheet_row_values = worksheet.row_values(actual_row_index)
             start_col = 15 - (tracked_cell.col % 2)
             for col_index in range(start_col, 1, -2):
-                if len(last_seven_days_values) == 7:
+                if count_days == 7:
                     break
                 if not worksheet_row_values[col_index]:
-                    last_seven_days_values.append(0)
+                    count_days += 1
                 else:
-                    last_seven_days_values.append(
-                        float(row_values[col_index].replace(',', '.')))
+                    last_seven_days_sum += float(row_values[col_index].replace(',', '.'))
+                    count_days_of_week_with_values += 1
+                    count_days += 1
             previous_worksheet_index += -1
-        count_days_of_week_with_values = sum((1 for day_value in last_seven_days_values if day_value != 0))
+    
         if count_days_of_week_with_values > 0:
-            average_previous_seven_days_sum = sum(last_seven_days_values) / count_days_of_week_with_values
+            average_previous_seven_days_sum = last_seven_days_sum / count_days_of_week_with_values
         return average_previous_seven_days_sum
 
-    def _average_difference_check(self, tracked_cell, user_name, app_name):
+    def average_difference_check(self, tracked_cell, user_name, app_name):
         ''' Сравнение  суммы из результатов парсинга и среднего значения за неделю.
        Если разница отличается на 10% ячейка изменяется ячейку
         '''
@@ -96,6 +100,6 @@ if __name__ == "__main__":
     tracking_fluctuation = TrackingFluctuations(
         key='1ZRMLXTClSNypxb3Lk_LkEYBAwDrnqD3-opeMwiOJ0AQ', cred_file_path=CRED)
     # for param_row in range(3, 13):
-    #     print(tracking_fluctuation._average_difference_check(tracking_fluctuation.actual_worksheet.cell(param_row, 9), 'Аккаунт 1', 'Название игры 1'))
+    #     print(tracking_fluctuation.average_difference_check(tracking_fluctuation.actual_worksheet.cell(param_row, 9), 'Аккаунт 1', 'Название игры 1'))
     tracking_fluctuation.actual_worksheet = tracking_fluctuation.sh.get_worksheet(1)
-    print(tracking_fluctuation._average_difference_check(tracking_fluctuation.actual_worksheet.acell('D34'), 'Аккаунт 1', 'Название игры 6'))
+    print(tracking_fluctuation.average_difference_check(tracking_fluctuation.actual_worksheet.acell('F56'), 'Аккаунт 5', 'Название игры 10'))
