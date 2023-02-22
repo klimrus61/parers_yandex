a7dc5821e812959fd995c4853766dd14d912eb2c
klimrus61 <klimrus61@mail.ru>
2023-02-22 09:40:29 +0300

    fix: Учтена разница в индексах колонок гугл таблицы и спика

--- tracking_fluctuations/tracking_fluctuations.py
+++ tracking_fluctuations/tracking_fluctuations.py
@@ -1,42 +1,66 @@
 import gspread
 
 
-SHEET_URL = '1ZRMLXTClSNypxb3Lk_LkEYBAwDrnqD3-opeMwiOJ0AQ'
-CRED = 'cred.json'
-
-
 class TrackingFluctuations():
 
     def __init__(self, key: str, cred_file_path: str ) -> None:
         self.CREDENTIALS_FILE = cred_file_path  # имя файла с закрытым ключом
         self.gc = gspread.service_account(filename=self.CREDENTIALS_FILE)
         self.sh = self.gc.open_by_key(key)
-        self.actual_worksheet = self.sh.get_worksheet(0)
+        self.actual_worksheet = self.sh.get_worksheet(2)
         self.sum_cells = 0
+        
+        self.ids_of_required_rows_for_worksheets = ['' for _ in range(len(self.sh.worksheets()))] # self.ids_of_required_rows[i] = нужная строка на i-том листе
 
+        
+    def get_last_seven_days_average_sum(self, tracked_cell, user_name: str, app_name: str) -> int:
 
-    def catch_fluctuations(self, tracked_cell):
+        average_previous_seven_days_sum = 0
+        last_seven_days_values = []
+        
+        previous_days = self.actual_worksheet.row_values(tracked_cell.row)
+        yesterday = tracked_cell.col - 2 - 1
+        for col_index in range(yesterday, 1, -2):
+            if not previous_days[col_index]:
+                continue
+            last_seven_days_values.append(float(previous_days[col_index].replace(',', '.')))
 
-         
+        previous_worksheet_index = self.actual_worksheet.index - 1
+
+        if len(last_seven_days_values) < 7:
+            while previous_worksheet_index >= 0 and len(last_seven_days_values) < 7:
+                worksheet = self.sh.get_worksheet(previous_worksheet_index)
+                user_and_app_names = worksheet.col_values(1)
+                if user_name not in user_and_app_names or app_name not in user_and_app_names:
+                    break
+                row_value = self.ids_of_required_rows_for_worksheets[previous_worksheet_index]
+                if not row_value:
+                    row_value = tracked_cell.row
+                worksheet_row_values = worksheet.row_values(row_value)
+                start_col = 15 - (tracked_cell.col % 2)
+                for col_index in range(start_col, 1, -2):
+                    if len(last_seven_days_values) == 7:
+                        break
+                    if not worksheet_row_values[col_index]:
+                        continue
+                    last_seven_days_values.append(float(previous_days[col_index].replace(',', '.')))
+                previous_worksheet_index += -1
+        if len(last_seven_days_values) > 0:
+            average_previous_seven_days_sum = sum(last_seven_days_values) / len(last_seven_days_values)
+        return average_previous_seven_days_sum
+
+                
+    def catch_fluctuations(self, tracked_cell, user_name, app_name):
+
         if tracked_cell.value:
             tracked_cell_value = float(tracked_cell.value.replace(',', '.'))
         else:
             return self.sum_cells
-        average_weekly_value = 0
-        numerator = 0
-        denominator = 0
 
-        previous_days = self.actual_worksheet.row_values(tracked_cell.row)
-        for i in range(tracked_cell.col -2, 1, -2):
-            if not previous_days[i]:
-                continue
-            numerator += float(previous_days[i].replace(',', '.'))
-            denominator += 1
-
-        if denominator != 0:
-            average_weekly_value = numerator / denominator
+        average_weekly_sum = self.get_last_seven_days_average_sum(tracked_cell, user_name, app_name)
+        
             
-        if tracked_cell_value > average_weekly_value * 1.1 and average_weekly_value > 0:
+        if tracked_cell_value > average_weekly_sum * 1.1 and average_weekly_sum > 0:
             self.actual_worksheet.format(tracked_cell.address,
             {"backgroundColor": {
                     "red": 0.0,
@@ -44,7 +68,7 @@ class TrackingFluctuations():
                     "blue": 0.0
             },})
             self.sum_cells += 1
-        elif tracked_cell_value < average_weekly_value * 0.9 and average_weekly_value > 0:
+        elif tracked_cell_value < average_weekly_sum * 0.9 and average_weekly_sum > 0:
             self.actual_worksheet.format(tracked_cell.address,
             {"backgroundColor": {
                     "red": 55.0,
@@ -58,7 +82,13 @@ class TrackingFluctuations():
 
 
 
+if __name__ == "__main__":
+    
+    SHEET_URL = '1ZRMLXTClSNypxb3Lk_LkEYBAwDrnqD3-opeMwiOJ0AQ'
+    CRED = 'tracking_fluctuations\cred.json'
 
-t = TrackingFluctuations(key=SHEET_URL, cred_file_path=CRED)
-for i in range(3, 17):
-    print(t.catch_fluctuations(t.actual_worksheet.cell(40, i)))
+    tracking_fluctuation = TrackingFluctuations(key='1ZRMLXTClSNypxb3Lk_LkEYBAwDrnqD3-opeMwiOJ0AQ', cred_file_path=CRED)
+    # for param_row in range(25, 35):
+    #     print(tracking_fluctuation.catch_fluctuations(tracking_fluctuation.actual_worksheet.cell(param_row, 4), 'Аккаунт 1', 'Название игры 1'))
+
+    print(tracking_fluctuation.catch_fluctuations(tracking_fluctuation.actual_worksheet.acell('O39'), 'Аккаунт 1', 'Название игры 1'))
