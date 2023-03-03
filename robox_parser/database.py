ead6260bb889b0170cfdeab59d57cde91aad60c8
klimrus61 <klimrus61@mail.ru>
2023-03-03 13:06:23 +0300

    refractor: вынесена проверка списка из метода, вместо цикла генератор

--- robox_parser/database.py
+++ robox_parser/database.py
@@ -17,30 +17,27 @@ class Database:
     def write_parse_data(self, data_to_game_table):
         '''Записывает спарсенные данные функцией get_data_from_roblox 
         в таблицу game базы данных roblox'''
-        if data_to_game_table:
-            try:
-                queries = []
-                for game in data_to_game_table:
-                    query = Game(
-                        group=game['group'],
-                        title=game['title'],
-                        active_players=game['active_players'],
-                        total_up_votes=game['total_up_votes'],
-                        total_down_votes=game['total_down_votes'],
-                        description=game['description'],
-                    )
-                    queries.append(query)
-                self.session.add_all(queries)
-                self.session.commit()
-                print('Данные успешно добавлены!')
-            except Exception as err:
-                self.session.rollback()
-                self.session.commit()
-                print(err)
-        else:
-            print('Передан пустой список')
+        try:
+            to_database = [Game(group=game['group'], \
+                                title=game['title'], \
+                                active_players=game['active_players'], \
+                                total_up_votes=game['total_up_votes'], \
+                                total_down_votes=game['total_down_votes'], \
+                                description=game['description'],) \
+                                for game in data_to_game_table]
+            self.session.add_all(to_database)
+            self.session.commit()
+            print('Данные успешно добавлены!')
+        except Exception as err:
+            self.session.rollback()
+            self.session.commit()
+            print(err)
+
 
 
 if __name__ == "__main__":
     data_to_game_table = get_data_from_roblox()
-    Database().write_parse_data(data_to_game_table)
+    if data_to_game_table:
+        Database().write_parse_data(data_to_game_table)
+    else:
+        print('Список для записи в базу пустой')
